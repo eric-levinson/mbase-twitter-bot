d09240e3b9f5e1ce899d53cd4ae8bfcad992e8f7
eric-levinson <eric.levinson42@gmail.com>
2017-11-20 17:46:29 -0600

    setup tweet user id filtering, so only op's in list get rt's and favs

--- src/bot.js
+++ src/bot.js
@@ -5,7 +5,6 @@ const bot = new Twit(config)
 tool = require('./sub/tweetinteract.js')
 
 //gets member list into array then forces to string
-//i don't want to talk about streaming.
 bot.get('lists/members', {
     slug: 'rt-list',
     owner_screen_name: 'mbase_eric'
@@ -13,46 +12,49 @@ bot.get('lists/members', {
     if (err) {
         console.log(err)
     } else {
+    	//array of user id's
         uData = []
         data.users.forEach(u => {
             uData.push(u.id_str)
         })
         uList = uData.toString()
-
+        //console.log(uList)
+        //stream of tweets from my list starts here.
         stream = bot.stream('statuses/filter', {
             follow: `${uList}`
         })
         stream.on('tweet', t => {
-
+        	//not real sure why this is sCloud but it shall remain for pull tweet urls..
+        	//the 't.entities.urls' is an array in an object in an array, lovely, i know..
             sCloud = t.entities.urls
-
             function findUrl(eurl) {
                 return eurl.url
             }
             sObj = sCloud.find(findUrl)
-            //console.log(sObj)
+
+            //Does tweet contain a URL?
             if (sCloud.length > 0) {
                 sArray = Object.values(sObj)
-                //console.log(sArray)
+                //tweet info
                 sString = sArray.toString()
                 tId = t.id_str
                 tText = t.text
                 tName = t.user.screen_name
 
+                //user cross ref info
+                idString = uData.toString()
+                idSimple = t.user.id_str
+
+                //url query parameters
                 rtArray = ['soundcloud','youtu.be','youtube','lilpeep','nojumper','itunes','spotify']
                 favArray = ['twimg','twitter.com/twitter/']
-
-                if (rtArray.some(function(v) { return sString.indexOf(v) >= 0 })) {
+                if (idString.indexOf(idSimple) >= 0 && rtArray.some(function(v) { return sString.indexOf(v) >= 0 })) {
                     tool.favTweet(),
                     tool.reTweet()
                 }
-                else if (favArray.some(function(v) { return sString.indexOf(v) >= 0 })) {
+                else if (idString.indexOf(idSimple) >= 0 && favArray.some(function(v) { return sString.indexOf(v) >= 0 })) {
                     tool.favTweet()
-                }/*
-                else if (sString.indexOf('youtube') >= 0){
-                    tool.favTweet(),
-                    tool.reTweet()
-                }*/
+                }
             }
         })
     }
