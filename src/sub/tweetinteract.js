932eb73a4ed69b2948f09a2983e58397775fd2ca
eric-levinson <eric.levinson42@gmail.com>
2017-11-30 14:09:40 -0600

    updated for user scripts

--- src/sub/tweetinteract.js
+++ src/sub/tweetinteract.js
@@ -11,6 +11,7 @@ module.exports = {
 	        (err, response) => {
 	            if (err) {
 	                console.log('error: unable to favorite: (probably already favorited..)'),
+	                //console.log(err)
 	                console.log(tText),
 	                console.log('author:', tName),
 	                console.log('tweet id:', tId),
@@ -39,6 +40,7 @@ module.exports = {
 	        (err, response) => {
 	            if (err) {
 	                console.log('error: unable to retweet: (probably already retweeted..)')
+	                //console.log(err)
 	                console.log(tText),
 	                console.log('author:', tName),
 	                console.log('tweet id:', tId),
@@ -54,5 +56,42 @@ module.exports = {
 	            }
 	        }
 		)
+	},
+	followBack: function(follower) {
+		const config = require('../config')
+	    const Twit = require('twit')
+	    const bot = new Twit(config)
+
+		followerUser = follower.source.id
+		followers_count = follower.source.followers_count
+		friends_count = follower.source.friends_count
+		verified = follower.source.verified
+		fRatio = followers_count / friends_count
+
+
+		if (fRatio >= 1 || verified === true){
+			return bot.post(
+		        'friendships/create', {
+		            user_id: followerUser
+		        },
+		        //error response code
+		        (err, response) => {
+		            if (err) {
+		                console.log('error: unable to follow user'),
+		                console.log(err)
+		                console.log('display name:', follower.source.name),
+		                console.log('handle:', follower.source.screen_name),
+		                console.log('\n')
+		            }
+		            //success code
+		            else {
+		                console.log('successfully followed user')
+		                console.log('display name:', follower.source.name),
+		                console.log('handle:', follower.source.screen_name),
+		                console.log('\n')
+		            }
+		        }
+	        )
+		}
 	}
 }
